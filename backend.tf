b91a2f5dd27ac2b6c0abcce85e42d7dc165592ec
Ankit <157349284+ankit-80@users.noreply.github.com>
2025-12-27 18:19:16 +0530

    Update Terraform backend key in backend.tf

--- backend.tf
+++ backend.tf
@@ -1,9 +1,10 @@
 terraform {
   backend "s3" {
     bucket = "hdfc-mobilebanking-prod-mum-01"
-    key = "main"
+    key = "ankit"
     region = "us-east-1"
     dynamodb_table = "ankit-dynamodb"
   }
 }
+
 
