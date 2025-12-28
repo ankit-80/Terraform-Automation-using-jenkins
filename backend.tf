cb54887f09ca9aa63ffdb6ae101f05f9db470410
Ankit <157349284+ankit-80@users.noreply.github.com>
2025-12-28 08:36:47 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,11 +1,12 @@
 terraform {
   backend "s3" {
     bucket = "hdfc-mobilebanking-prod-mum-01"
-    key = "ankit/terraform.tfstate"
+    key = "jenkins_data/terraform.tfstate"
     region = "us-east-1"
-    dynamodb_table = "ankit-dynamodb"
+    dynamodb_table = "my-terraform-lock-table"
   }
 }
+
 
 
 
