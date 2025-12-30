0eec70c42e4da68113be3bbead8f8f5010f211b4
Ankit <157349284+ankit-80@users.noreply.github.com>
2025-12-30 08:16:37 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,4 +1,4 @@
-/* variable "aws_region" {
+variable "aws_region" {
        description = "The AWS region to create things in." 
        default     = "us-east-1" 
 }
@@ -49,5 +49,6 @@ variable "tags" {
         terraform   = "true"
     }
 }
-*/
+
+
 
